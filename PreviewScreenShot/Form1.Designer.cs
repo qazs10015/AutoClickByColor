fa293aa184bda26b0b59d575b1ec62ea0f99e251
junyu <fly12610@gmail.com>
2020-12-13 23:05:06 +0800

    first commit

---
+++ PreviewScreenShot/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PreviewScreenShot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCoordinate = new System.Windows.Forms.Label();
+            this.lblColorName = new System.Windows.Forms.Label();
+            this.scrollYaxis = new System.Windows.Forms.HScrollBar();
+            this.txtYaxis = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnExit.Location = new System.Drawing.Point(486, 151);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(96, 26);
+            this.btnExit.TabIndex = 22;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.Location = new System.Drawing.Point(486, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 24;
+            this.checkBox1.Text = "Start";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(152, 146);
+            this.panel1.TabIndex = 30;
+            // 
+            // lblCoordinate
+            // 
+            this.lblCoordinate.AutoSize = true;
+            this.lblCoordinate.Location = new System.Drawing.Point(387, 75);
+            this.lblCoordinate.Name = "lblCoordinate";
+            this.lblCoordinate.Size = new System.Drawing.Size(0, 12);
+            this.lblCoordinate.TabIndex = 0;
+            // 
+            // lblColorName
+            // 
+            this.lblColorName.AutoSize = true;
+            this.lblColorName.Location = new System.Drawing.Point(387, 107);
+            this.lblColorName.Name = "lblColorName";
+            this.lblColorName.Size = new System.Drawing.Size(0, 12);
+            this.lblColorName.TabIndex = 31;
+            // 
+            // scrollYaxis
+            // 
+            this.scrollYaxis.Location = new System.Drawing.Point(262, 11);
+            this.scrollYaxis.Maximum = 150;
+            this.scrollYaxis.Name = "scrollYaxis";
+            this.scrollYaxis.Size = new System.Drawing.Size(111, 17);
+            this.scrollYaxis.TabIndex = 32;
+            this.scrollYaxis.Value = 10;
+            this.scrollYaxis.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
+            // 
+            // txtYaxis
+            // 
+            this.txtYaxis.Location = new System.Drawing.Point(211, 7);
+            this.txtYaxis.Name = "txtYaxis";
+            this.txtYaxis.Size = new System.Drawing.Size(36, 22);
+            this.txtYaxis.TabIndex = 33;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(389, 6);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(52, 23);
+            this.btnUpdate.TabIndex = 34;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(594, 197);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtYaxis);
+            this.Controls.Add(this.scrollYaxis);
+            this.Controls.Add(this.lblColorName);
+            this.Controls.Add(this.lblCoordinate);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Blue;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCoordinate;
+        private System.Windows.Forms.Label lblColorName;
+        private System.Windows.Forms.HScrollBar scrollYaxis;
+        private System.Windows.Forms.TextBox txtYaxis;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ PreviewScreenShot/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PreviewScreenShot
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int LEFTUP = 0x0004;
+        private const int LEFTDOWN = 0x0002;
+        private Boolean isClick = false;
+        public bool clickble = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.Opacity = .5D; //Make trasparent
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true); // this is to avoid visual artifacts
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            //取得設定檔
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            //取得設定檔中的屬性資料
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("detectYaxis")))
+            {
+                txtYaxis.Text = config.AppSettings.Settings["detectYaxis"].Value;
+                scrollYaxis.Value = Convert.ToInt32(config.AppSettings.Settings["detectYaxis"].Value);
+            }
+            else
+            {
+                config.AppSettings.Settings.Add("detectYaxis", "10");
+                scrollYaxis.Value = 10;
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+
+            
+            backgroundWorker1.RunWorkerAsync();
+
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            backgroundWorker1.Dispose();
+            Environment.Exit(Environment.ExitCode);
+        }
+        // 滑鼠點擊事件
+        private void MouseClickAction()
+        {
+
+            mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+            Thread.Sleep(1);
+            mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+            Thread.Sleep(10);
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void OnPaint(PaintEventArgs e) // you can safely omit this method if you want
+        {
+            //e.Graphics.FillRectangle(Brushes.Green, Top);
+            //e.Graphics.FillRectangle(Brushes.Green, Left);
+            //e.Graphics.FillRectangle(Brushes.Green, Right);
+            //e.Graphics.FillRectangle(Brushes.Green, Bottom);
+        }
+
+        // 背景執行事件
+        private void BackgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (!string.IsNullOrEmpty(txtYaxis.Text))
+            {
+                Color color = GrabColor();
+
+                // 勾選之後才能執行以下動作
+                if (checkBox1.Checked)
+                {
+                    // 偵測綠區，如果有偵測到時就滑鼠點擊並睡5秒
+                    if ((color.R > 160 && color.G > 190 && color.B > 90 && color.B < 150))
+                    {
+
+                        Console.WriteLine("detect");
+                        Console.WriteLine(color.Name);
+                        MouseClickAction();
+                        isClick = true;
+                        Thread.Sleep(5000);
+
+                    }
+                    // 當isClick設為true，就會執行滑鼠點擊功能
+                    if (isClick)
+                    {
+                        Console.WriteLine("restart");
+                        MouseClickAction();
+                        isClick = false;
+                    }
+                }
+                else
+                {
+                    isClick = false;
+                }
+
+                Thread.Sleep(5);
+            }
+        }
+        
+        // 取得顏色
+        Color GrabColor()
+        {
+            Pen pen = new Pen(Color.Red, 0.5f);
+            Bitmap bmp = new Bitmap(panel1.Width, panel1.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.Clear(Color.Gray);
+            graphics.CopyFromScreen(
+                panel1.RectangleToScreen(panel1.ClientRectangle).Location,
+                Point.Empty,
+                panel1.Size);
+
+            Graphics focus = panel1.CreateGraphics();
+            focus.Clear(Color.Blue);
+            focus.DrawRectangle(pen, panel1.Width / 2 - 5, scrollYaxis.Value - 5, 7, 7);
+
+            Color color = bmp.GetPixel(panel1.Width / 2, scrollYaxis.Value);
+            lblCoordinate.Text = color.ToString();
+            lblColorName.Text = color.Name;
+            lblCoordinate.BackColor = color;
+
+            // panel2.BackgroundImage = bmp;
+
+
+
+            //panel2.BackColor = pixel;
+            //pictureBox1.Image = bmp;
+            //pictureBox1.Update();
+            //pictureBox2.BackColor = pixel;
+            return color;
+        }
+    
+        private void HScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            txtYaxis.Text = scrollYaxis.Value.ToString();
+            Console.WriteLine(txtYaxis.Text);
+        }
+        // 更新設定檔的屬性資料
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            config.AppSettings.Settings["detectYaxis"].Value = txtYaxis.Text;
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
